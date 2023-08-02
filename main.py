a385f6b942e83bb7886c6d61a80b62b76bf9caac
dmitry <kovtunov.da@students.dvfu.ru>
2023-08-02 15:18:55 +1000

    add polygon sides sizes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, send_file, redirect, url_for
-import cairo
+from funcs import *
 
 app = Flask(__name__, template_folder='templates')
 app.secret_key = 'bezbozhiye botforty vyvernutsya doslushat kibernetik predrassudok protivoyadiye raskrovenit slyapat'
@@ -83,9 +83,11 @@ def save_project():
     for poly in polygons:
         for point_pair in poly:
             ctx.line_to(point_pair[0], point_pair[1])
-    ctx.set_source_rgb(0, 0, 0)
-    ctx.set_line_width(size / 400)
-    ctx.stroke()
+
+        ctx.set_source_rgb(0, 0, 0)
+        ctx.set_line_width(size / 400)
+        ctx.stroke()
+        ctx.close_path()
 
     save_surface.finish()
     save_surface.flush()
@@ -119,27 +121,14 @@ def add_polygon1():
                 ctx.set_source_rgb(0.5, 0.5, 0.5)
                 ctx.set_line_width(size / 400)
                 ctx.stroke()
+                ctx.close_path()
 
                 ctx.line_to(center, 0)
                 ctx.line_to(center, size)
                 ctx.set_source_rgb(0.5, 0.5, 0.5)
                 ctx.set_line_width(size / 400)
                 ctx.stroke()
-
-                for poly in polygons:
-                    for point_pair in poly:
-                        ctx.line_to(point_pair[0], point_pair[1])
-                ctx.set_source_rgb(0, 0, 0)
-                ctx.set_line_width(size / 400)
-                ctx.stroke()
-
-                ctx.set_source_rgb(0, 0, 0)
-                ctx.rectangle(image_center[0] - width // 2, image_center[1] - y2, width, y1 + y2)
-                ctx.set_line_width(size / 400)
-                ctx.stroke()
-
-                surface.finish()
-                surface.flush()
+                ctx.close_path()
 
                 polygons.append([(image_center[0] + width // 2, image_center[1] + y1),
                                  (image_center[0] - width // 2, image_center[1] + y1),
@@ -148,6 +137,38 @@ def add_polygon1():
                                  (image_center[0] + width // 2, image_center[1] + y1)])
                 points.append(
                     str(f'{(width // 2, y1)}, {(-width // 2, y1)}, {(-width // 2, -y2)}, {(width // 2, -y2)}'))
+
+                for poly in polygons:
+                    for point_pair in poly:
+                        ctx.line_to(point_pair[0], point_pair[1])
+
+                    ctx.set_source_rgb(0, 0, 0)
+                    ctx.set_line_width(size / 400)
+                    ctx.stroke()
+                    ctx.close_path()
+
+                for poly in polygons:
+                    if len(poly) == 5:
+                        center1 = (poly[1][0] - (poly[1][0] - poly[0][0]) / 2,
+                                   poly[1][1] - (poly[1][1] - poly[0][1]) / 2)
+                        center2 = (poly[2][0] - (poly[2][0] - poly[1][0]) / 2,
+                                   poly[2][1] - (poly[2][1] - poly[1][1]) / 2)
+                        center3 = (poly[3][0] - (poly[3][0] - poly[2][0]) / 2,
+                                   poly[3][1] - (poly[3][1] - poly[2][1]) / 2)
+                        center4 = (poly[4][0] - (poly[4][0] - poly[3][0]) / 2,
+                                   poly[4][1] - (poly[4][1] - poly[3][1]) / 2)
+
+                        text(ctx, str(calc_distance(poly[0], poly[1])), center1, calc_angle(poly[0], poly[1]),
+                             font_size=10)
+                        text(ctx, str(calc_distance(poly[2], poly[1])), center2, calc_angle(poly[1], poly[2]),
+                             font_size=10)
+                        text(ctx, str(calc_distance(poly[3], poly[2])), center3, calc_angle(poly[2], poly[3]),
+                             font_size=10)
+                        text(ctx, str(calc_distance(poly[4], poly[3])), center4, calc_angle(poly[3], poly[4]),
+                             font_size=10)
+
+                surface.finish()
+                surface.flush()
 
         except Exception:
             pass
@@ -191,39 +212,50 @@ def add_polygon2():
                     ctx.set_source_rgb(0.5, 0.5, 0.5)
                     ctx.set_line_width(size / 400)
                     ctx.stroke()
-
-                    for poly in polygons:
-                        for point_pair in poly:
-                            ctx.line_to(point_pair[0], point_pair[1])
-                    ctx.set_source_rgb(0, 0, 0)
-                    ctx.set_line_width(size / 400)
-                    ctx.stroke()
+                    ctx.close_path()
 
                     ctx.line_to(center, 0)
                     ctx.line_to(center, size)
                     ctx.set_source_rgb(0.5, 0.5, 0.5)
                     ctx.set_line_width(size / 400)
                     ctx.stroke()
+                    ctx.close_path()
 
-                    ctx.set_source_rgb(0, 0, 0)
-                    ctx.line_to(image_center[0] + x1, image_center[1] - y1)
-                    ctx.line_to(image_center[0] - x2, image_center[1] - y2)
-                    ctx.line_to(image_center[0] - x3, image_center[1] + y3)
-                    ctx.line_to(image_center[0] + x4, image_center[1] + y4)
-                    ctx.line_to(image_center[0] + x1, image_center[1] - y1)
-                    ctx.set_line_width(size / 400)
-                    ctx.stroke()
+                    polygons.append([(image_center[0] + x1, image_center[1] - y1),
+                                     (image_center[0] - x2, image_center[1] - y2),
+                                     (image_center[0] - x3, image_center[1] + y3),
+                                     (image_center[0] + x4, image_center[1] + y4),
+                                     (image_center[0] + x1, image_center[1] - y1)])
+                    points.append(
+                        str(f'{(x1, y1)}, {(-x2, y2)}, {(-x3, -y3)}, {(x4, -y4)}'))
+
+                    for poly in polygons:
+                        for point_pair in poly:
+                            ctx.line_to(point_pair[0], point_pair[1])
+
+                        ctx.set_source_rgb(0, 0, 0)
+                        ctx.set_line_width(size / 400)
+                        ctx.stroke()
+                        ctx.close_path()
+
+                    for poly in polygons:
+                        if len(poly) == 5:
+                            center1 = (poly[1][0] - (poly[1][0] - poly[0][0]) / 2,
+                                       poly[1][1] - (poly[1][1] - poly[0][1]) / 2)
+                            center2 = (poly[2][0] - (poly[2][0] - poly[1][0]) / 2,
+                                       poly[2][1] - (poly[2][1] - poly[1][1]) / 2)
+                            center3 = (poly[3][0] - (poly[3][0] - poly[2][0]) / 2,
+                                       poly[3][1] - (poly[3][1] - poly[2][1]) / 2)
+                            center4 = (poly[4][0] - (poly[4][0] - poly[3][0]) / 2,
+                                       poly[4][1] - (poly[4][1] - poly[3][1]) / 2)
+
+                            text(ctx, str(calc_distance(poly[0], poly[1])), center1, calc_angle(poly[0], poly[1]), font_size=10)
+                            text(ctx, str(calc_distance(poly[2], poly[1])), center2, calc_angle(poly[1], poly[2]), font_size=10)
+                            text(ctx, str(calc_distance(poly[3], poly[2])), center3, calc_angle(poly[2], poly[3]), font_size=10)
+                            text(ctx, str(calc_distance(poly[4], poly[3])), center4, calc_angle(poly[3], poly[4]), font_size=10)
 
                     surface.finish()
                     surface.flush()
-
-                    polygons.append([(image_center[0] + x1, image_center[1] + y1),
-                                     (image_center[0] - x2, image_center[1] + y2),
-                                     (image_center[0] - x3, image_center[1] - y3),
-                                     (image_center[0] + x4, image_center[1] - y4),
-                                     (image_center[0] + x1, image_center[1] + y1)])
-                    points.append(
-                        str(f'{(x1, y1)}, {(-x2, y2)}, {(-x3, -y3)}, {(x4, -y4)}'))
 
         except Exception:
             pass

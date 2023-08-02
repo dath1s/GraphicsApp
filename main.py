8b22545ff43fc95668e4e4dc3e6c9f27a07c958b
dmitry <kovtunov.da@students.dvfu.ru>
2023-08-02 20:54:08 +1000

    add save page

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 from flask import Flask, render_template, request, send_file, redirect, url_for
 from funcs import *
+import svgutils.transform as sg
 
 app = Flask(__name__, template_folder='templates')
 app.secret_key = 'bezbozhiye botforty vyvernutsya doslushat kibernetik predrassudok protivoyadiye raskrovenit slyapat'
 polygons = []
 points = []
 
-last_size = 0
-last_polygon_type = 1
-
 size = 550
 center = size // 2
+field_size = 550
 
 
 @app.route('/')
@@ -71,7 +70,7 @@ def index():
 
 @app.route('/save_project', methods=['GET', 'POST'])
 def save_project():
-    global polygons
+    global polygons, size, center
 
     save_surface = cairo.SVGSurface('static/img/save_polygon.svg', size, size)
     ctx = cairo.Context(save_surface)
@@ -82,7 +81,7 @@ def save_project():
 
     for poly in polygons:
         for point_pair in poly:
-            ctx.line_to(point_pair[0], point_pair[1])
+            ctx.line_to(center + point_pair[0], center + point_pair[1])
 
         ctx.set_source_rgb(0, 0, 0)
         ctx.set_line_width(size / 400)
@@ -97,8 +96,7 @@ def save_project():
 
 @app.route('/add_polygon1', methods=['GET', 'POST'])
 def add_polygon1():
-    global last_size, last_polygon_type, points, polygons
-    last_polygon_type = 1
+    global points, polygons, size, center
 
     if request.method == 'POST':
         try:
@@ -106,7 +104,8 @@ def add_polygon1():
             if all([i is not None for i in [width, y1, y2]]):
                 width, y1, y2 = int(width), int(y1), int(y2)
 
-                image_center = (center, center)
+                size = max([size, width * 1.2, (y1 + y2) * 1.2])
+                center = size / 2
 
                 surface = cairo.SVGSurface('static/img/polygon.svg', size, size)
 
@@ -130,17 +129,17 @@ def add_polygon1():
                 ctx.stroke()
                 ctx.close_path()
 
-                polygons.append([(image_center[0] + width // 2, image_center[1] + y1),
-                                 (image_center[0] - width // 2, image_center[1] + y1),
-                                 (image_center[0] - width // 2, image_center[1] - y2),
-                                 (image_center[0] + width // 2, image_center[1] - y2),
-                                 (image_center[0] + width // 2, image_center[1] + y1)])
+                polygons.append([(width // 2, y1),
+                                 (-width // 2, y1),
+                                 (-width // 2, -y2),
+                                 (width // 2, -y2),
+                                 (width // 2, y1)])
                 points.append(
                     str(f'{(width // 2, y1)}, {(-width // 2, y1)}, {(-width // 2, -y2)}, {(width // 2, -y2)}'))
 
                 for poly in polygons:
                     for point_pair in poly:
-                        ctx.line_to(point_pair[0], point_pair[1])
+                        ctx.line_to(center + point_pair[0], center + point_pair[1])
 
                     ctx.set_source_rgb(0, 0, 0)
                     ctx.set_line_width(size / 400)
@@ -149,26 +148,29 @@ def add_polygon1():
 
                 for poly in polygons:
                     if len(poly) == 5:
-                        center1 = (poly[1][0] - (poly[1][0] - poly[0][0]) / 2,
-                                   poly[1][1] - (poly[1][1] - poly[0][1]) / 2)
-                        center2 = (poly[2][0] - (poly[2][0] - poly[1][0]) / 2,
-                                   poly[2][1] - (poly[2][1] - poly[1][1]) / 2)
-                        center3 = (poly[3][0] - (poly[3][0] - poly[2][0]) / 2,
-                                   poly[3][1] - (poly[3][1] - poly[2][1]) / 2)
-                        center4 = (poly[4][0] - (poly[4][0] - poly[3][0]) / 2,
-                                   poly[4][1] - (poly[4][1] - poly[3][1]) / 2)
+                        center1 = (center + poly[1][0] - (poly[1][0] - poly[0][0]) / 2,
+                                   center + poly[1][1] - (poly[1][1] - poly[0][1]) / 2)
+                        center2 = (center + poly[2][0] - (poly[2][0] - poly[1][0]) / 2,
+                                   center + poly[2][1] - (poly[2][1] - poly[1][1]) / 2)
+                        center3 = (center + poly[3][0] - (poly[3][0] - poly[2][0]) / 2,
+                                   center + poly[3][1] - (poly[3][1] - poly[2][1]) / 2)
+                        center4 = (center + poly[4][0] - (poly[4][0] - poly[3][0]) / 2,
+                                   center + poly[4][1] - (poly[4][1] - poly[3][1]) / 2)
 
                         text(ctx, str(calc_distance(poly[0], poly[1])), center1, calc_angle(poly[0], poly[1]),
-                             font_size=10)
+                             font_size=size // 55)
                         text(ctx, str(calc_distance(poly[2], poly[1])), center2, calc_angle(poly[1], poly[2]),
-                             font_size=10)
+                             font_size=size // 55)
                         text(ctx, str(calc_distance(poly[3], poly[2])), center3, calc_angle(poly[2], poly[3]),
-                             font_size=10)
+                             font_size=size // 55)
                         text(ctx, str(calc_distance(poly[4], poly[3])), center4, calc_angle(poly[3], poly[4]),
-                             font_size=10)
+                             font_size=size // 55)
 
                 surface.finish()
                 surface.flush()
+                fig = sg.fromfile('static/img/polygon.svg')
+                fig.set_size((f'{field_size}', f'{field_size}'))
+                fig.save('static/img/polygon.svg')
 
         except Exception:
             pass
@@ -179,9 +181,8 @@ def add_polygon1():
 
 @app.route('/add_polygon2', methods=['POST'])
 def add_polygon2():
-    global last_size, last_polygon_type, points, polygons
+    global points, polygons, size, center
 
-    last_polygon_type = 2
     if request.method == 'POST':
         try:
             x1, x2, x3, x4, y1, y2, y3, y4 = [request.form.get(i) for i in
@@ -196,8 +197,10 @@ def add_polygon2():
                 y3 = int(y3)
                 y4 = int(y4)
 
+                size = max([size, x1 * 2.4, x2 * 2.4, x3 * 2.4, x4 * 2.4, y1 * 2.4, y2 * 2.4, y3 * 2.4, y4 * 2.4])
+                center = size / 2
+
                 if all([i >= 0 for i in [x1, x2, x3, x4, y1, y2, y3, y4]]):
-                    image_center = (center, center)
 
                     surface = cairo.SVGSurface('static/img/polygon.svg', size, size)
 
@@ -221,17 +224,17 @@ def add_polygon2():
                     ctx.stroke()
                     ctx.close_path()
 
-                    polygons.append([(image_center[0] + x1, image_center[1] - y1),
-                                     (image_center[0] - x2, image_center[1] - y2),
-                                     (image_center[0] - x3, image_center[1] + y3),
-                                     (image_center[0] + x4, image_center[1] + y4),
-                                     (image_center[0] + x1, image_center[1] - y1)])
+                    polygons.append([(x1, -y1),
+                                     (-x2, -y2),
+                                     (-x3, y3),
+                                     (x4, y4),
+                                     (x1, -y1)])
                     points.append(
                         str(f'{(x1, y1)}, {(-x2, y2)}, {(-x3, -y3)}, {(x4, -y4)}'))
 
                     for poly in polygons:
                         for point_pair in poly:
-                            ctx.line_to(point_pair[0], point_pair[1])
+                            ctx.line_to(center + point_pair[0], center + point_pair[1])
 
                         ctx.set_source_rgb(0, 0, 0)
                         ctx.set_line_width(size / 400)
@@ -240,23 +243,30 @@ def add_polygon2():
 
                     for poly in polygons:
                         if len(poly) == 5:
-                            center1 = (poly[1][0] - (poly[1][0] - poly[0][0]) / 2,
-                                       poly[1][1] - (poly[1][1] - poly[0][1]) / 2)
-                            center2 = (poly[2][0] - (poly[2][0] - poly[1][0]) / 2,
-                                       poly[2][1] - (poly[2][1] - poly[1][1]) / 2)
-                            center3 = (poly[3][0] - (poly[3][0] - poly[2][0]) / 2,
-                                       poly[3][1] - (poly[3][1] - poly[2][1]) / 2)
-                            center4 = (poly[4][0] - (poly[4][0] - poly[3][0]) / 2,
-                                       poly[4][1] - (poly[4][1] - poly[3][1]) / 2)
+                            center1 = (center + poly[1][0] - (poly[1][0] - poly[0][0]) / 2,
+                                       center + poly[1][1] - (poly[1][1] - poly[0][1]) / 2)
+                            center2 = (center + poly[2][0] - (poly[2][0] - poly[1][0]) / 2,
+                                       center + poly[2][1] - (poly[2][1] - poly[1][1]) / 2)
+                            center3 = (center + poly[3][0] - (poly[3][0] - poly[2][0]) / 2,
+                                       center + poly[3][1] - (poly[3][1] - poly[2][1]) / 2)
+                            center4 = (center + poly[4][0] - (poly[4][0] - poly[3][0]) / 2,
+                                       center + poly[4][1] - (poly[4][1] - poly[3][1]) / 2)
 
-                            text(ctx, str(calc_distance(poly[0], poly[1])), center1, calc_angle(poly[0], poly[1]), font_size=10)
-                            text(ctx, str(calc_distance(poly[2], poly[1])), center2, calc_angle(poly[1], poly[2]), font_size=10)
-                            text(ctx, str(calc_distance(poly[3], poly[2])), center3, calc_angle(poly[2], poly[3]), font_size=10)
-                            text(ctx, str(calc_distance(poly[4], poly[3])), center4, calc_angle(poly[3], poly[4]), font_size=10)
+                            text(ctx, str(calc_distance(poly[0], poly[1])), center1, calc_angle(poly[0], poly[1]),
+                                 font_size=size // 55)
+                            text(ctx, str(calc_distance(poly[2], poly[1])), center2, calc_angle(poly[1], poly[2]),
+                                 font_size=size // 55)
+                            text(ctx, str(calc_distance(poly[3], poly[2])), center3, calc_angle(poly[2], poly[3]),
+                                 font_size=size // 55)
+                            text(ctx, str(calc_distance(poly[4], poly[3])), center4, calc_angle(poly[3], poly[4]),
+                                 font_size=size // 55)
 
                     surface.finish()
                     surface.flush()
 
+                    fig = sg.fromfile('static/img/polygon.svg')
+                    fig.set_size((f'{field_size}', f'{field_size}'))
+                    fig.save('static/img/polygon.svg')
         except Exception:
             pass
         return render_template('index.html', polygon_type=2, start=0 if len(points) <= 16 else len(points) - 16,
@@ -265,7 +275,9 @@ def add_polygon2():
 
 @app.route('/reset_project', methods=['POST'])
 def reset_project():
-    global polygons, points
+    global polygons, points, size, center
+    size = field_size
+    center = size / 2
 
     polygons = []
     points = []
@@ -294,6 +306,16 @@ def reset_project():
     surface.flush()
 
     return redirect(url_for('add_polygon1'))
+
+
+@app.route('/save', methods=['GET', 'POST'])
+def save():
+    if request.method == 'POST':
+        customer, time, executor = request.form.get('customer'), request.form.get('time'), request.form.get('executor')
+        if all([i is not None for i in [customer, time, executor]]):
+            print(customer, time, executor)
+
+    return render_template('save_page.html')
 
 
 if __name__ == "__main__":
